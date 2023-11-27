3f4b23a510f05c27e62767368ccd75ad926234cb
Hugo Barrera <exailer89@gmail.com>
2023-11-26 22:38:33 -0600

    Creando e instanciando la clase de Presupuesto

--- js/app.js
+++ js/app.js
@@ -11,16 +11,32 @@ function eventListeners() {
 
 
 // Clases
+class Presupuesto {
+    constructor(presupuesto) {
+        this.presupuesto = Number(presupuesto);
+        this.restante = Number(presupuesto);
+        this.gastos = [];
+    }
+}
+
+class UI {
+
+}
 
 
+// Instanciar
+const ui = new UI();
+let presupuesto;
 
 // Funciones
 function preguntarPresupuesto() {
     const presupuestoUsurio = prompt('¿Cual es tu presupuesto?');
 
-    console.log(Number(presupuestoUsurio));
-
     if (presupuestoUsurio === '' || presupuestoUsurio === null || isNaN(presupuestoUsurio) || presupuestoUsurio <= 0) { // isNaN es una función de JS que significar Is Not a Number.
         window.location.reload();
     }
+
+    // Presupuesto valido
+    presupuesto = new Presupuesto(presupuestoUsurio);
+    console.log(presupuesto);
 }
