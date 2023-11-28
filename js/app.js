f736c4fb4e3f5d8205105703cfd0b182f5b554bc
Hugo Barrera <exailer89@gmail.com>
2023-11-27 20:56:36 -0600

    Trabajando con la clase de UI

--- js/app.js
+++ js/app.js
@@ -20,7 +20,14 @@ class Presupuesto {
 }
 
 class UI {
+    insertarPresupuesto(cantidad) {
+        // Extrayendo los valores
+        const { presupuesto, restante } = cantidad;
 
+        // Agregar al HTML
+        document.querySelector('#total').textContent = presupuesto;
+        document.querySelector('#restante').textContent = restante;
+    }
 }
 
 
@@ -39,4 +46,6 @@ function preguntarPresupuesto() {
     // Presupuesto valido
     presupuesto = new Presupuesto(presupuestoUsurio);
     console.log(presupuesto);
+
+    ui.insertarPresupuesto(presupuesto);
 }
